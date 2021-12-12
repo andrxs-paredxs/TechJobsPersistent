6df296dc33e8c89bdf30f0323694ad2e1c8ec3e5
Andres <andres.paredes@protonmail.com>
2021-12-12 10:57:52 -0500

    EmployerController Part 1,2 and 3

--- TechJobsPersistent/Controllers/EmployerController.cs
+++ TechJobsPersistent/Controllers/EmployerController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using TechJobsPersistent.Data;
 using TechJobsPersistent.Models;
 using TechJobsPersistent.ViewModels;
 
@@ -10,12 +11,20 @@ using TechJobsPersistent.ViewModels;
 
 namespace TechJobsPersistent.Controllers
 {
+   
     public class EmployerController : Controller
     {
+
+        private JobDbContext context;
+
+        public EmployerController(JobDbContext dbContext) {
+            context = dbContext;
+        }
+
         // GET: /<controller>/
         public IActionResult Index()
         {
-            List<Employer> employers = new List<Employer>();//list of employers
+            List<Employer> employers = context.Employers.ToList();//new List<Employer>();//list of employers
             return View(employers);
         }
 
@@ -24,6 +33,7 @@ namespace TechJobsPersistent.Controllers
             AddEmployerViewModel addEmployerViewModel = new AddEmployerViewModel();
             return View(addEmployerViewModel);
         }
+
         //[httppsot] model binding Employer
         [HttpPost]
         [Route("/add")]
@@ -37,8 +47,9 @@ namespace TechJobsPersistent.Controllers
                     Location = addEmployerViewModel.Location
                 };
 
-                //TODO: Add this new employer into the DB
-                //return View();
+                context.Employers.Add(newEmployer);
+                context.SaveChanges();
+                
                 return Redirect("/Employer");
             }
 
@@ -47,7 +58,8 @@ namespace TechJobsPersistent.Controllers
 
         public IActionResult About(int id)
         {
-            return View();
+            Employer employer = context.Employers.Find(id);//list of employers
+            return View(employer);
         }
     }
 }
